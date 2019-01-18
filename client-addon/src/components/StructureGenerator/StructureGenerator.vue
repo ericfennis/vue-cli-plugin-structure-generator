175569325be805d9865f5671b241cf4ad832f54f
Eric Fennis <eric.fennis@mediamonks.com>
2019-01-18 12:02:13 +0100

    Added task en view components

--- client-addon/src/components/StructureGenerator/StructureGenerator.vue
+++
@@ -1,9 +0,0 @@
-<style src="./StructureGenerator.scss" lang="scss" scoped></style>
-<script src="./StructureGenerator.js"></script>
-
-<template>
-  <div class="example-component">
-    <h3>Example Component</h3>
-
-  </div>
-</template>
--- client-addon/src/main.js
+++ client-addon/src/main.js
@@ -1,3 +1,7 @@
 import StructureGenerator from './components/StructureGenerator'
 
 ClientAddonApi.component('org.vue.webpack.vue-cli-plugin-structure-generator', StructureGenerator)
+
+ClientAddonApi.addRoutes('org.vue.webpack', [
+  { path: '/generator', name: 'org.vue.webpack.routes.test', component: TestView }
+])
